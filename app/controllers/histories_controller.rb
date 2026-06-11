2c95928700d95e6a325c8c615107a06b30a75bfa
Jucaski <thanididbefore@gmail.com>
2026-06-11 08:00:47 -0600

    showing notes in the calendar

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,7 +2,7 @@ class HistoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @sessions_by_date = current_user.workout_sessions.group_by(&:date)
-    @date = params[:date] ? Date.parse(params[:date]) : Date.current
-  end
+  @sessions = current_user.workout_sessions.order(:date)
+  @date = params[:date] ? Date.parse(params[:date]) : Date.current
+end
 end
--- app/models/workout_session.rb
+++ app/models/workout_session.rb
@@ -4,4 +4,7 @@ class WorkoutSession < ApplicationRecord
 
   validates :date, :sets, :work_duration, :rest_duration, presence: true
   validates :is_running, inclusion: { in: [true, false] }
+  def start_time
+    date
+  end
 end
