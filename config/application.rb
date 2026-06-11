9be24bab177f1bdced8f50cdcec3be1c82be7b11
Jucaski <thanididbefore@gmail.com>
2026-06-11 08:51:43 -0600

    add timer in the navbar

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module GoalManager
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
     config.time_zone = ENV.fetch('TIME_ZONE', 'America/New_York')
+    config.beginning_of_week = :sunday
 
     # Configuration for the application, engines, and railties goes here.
     #
